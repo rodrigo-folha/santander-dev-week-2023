8dbbf6363d2f44221c1c56802d808cad60341802
Rodrigo <rodrigofomo@gmail.com>
2024-05-22 21:26:22 -0300

    Implementação da camada de login

--- src/main/java/me/dio/domain/model/Card.java
+++ src/main/java/me/dio/domain/model/Card.java
@@ -1,10 +1,18 @@
 package me.dio.domain.model;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity(name = "tb_card")
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String number;
+    @Column(name = "available_limit", scale = 13, precision = 2)
     private BigDecimal limit;
 
     public Long getId() {
--- src/main/java/me/dio/domain/model/User.java
+++ src/main/java/me/dio/domain/model/User.java
@@ -10,11 +10,16 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Account account;
+    @OneToOne(cascade = CascadeType.ALL)
     private Card card;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Feature> features;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<News> news;
 
     public Long getId() {
